6d4ab167f1b0fb4099cfdf97cc6224cbb39bb829
David Björklund <david.bjorklund@gmail.com>
2016-02-29 21:46:55 +0100

    add readme

---
+++ example.js
@@ -0,0 +1,12 @@
+import createQuery from 'levelgraph-query';
+import levelgraph from 'levelgraph';
+import level from 'level';
+
+const db = levelgraph(level('yourdb'));
+const query = createQuery(query);
+
+// :subject -> db.v('subject')
+// so the query below is translated to
+// [{ subject: db.v('subject'), predicate: 'predicate', object: object }]
+// so query(...) can be used in db.searchStream and/or db.search
+db.searchStream(query(':subject -[predicate]-> object'));
